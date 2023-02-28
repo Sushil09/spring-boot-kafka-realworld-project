886c81e2398385de732fe34ecf3253c7033ace0a
Sushil Jaiswal <sushil.jaiswal@oracle.com>
2023-03-01 00:05:26 +0530

    Initial commit

---
+++ src/main/java/com/sjsushil09/kafka/SpringBootKafkaRelaworldProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sjsushil09.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaRelaworldProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootKafkaRelaworldProjectApplication.class, args);
+    }
+
+}
